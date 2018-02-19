6abc048ed692762a4fa9bd683732d1de29176d0b
Arpith Siromoney <arpith@feedreader.co>
2018-02-19 16:29:20 -0500

    kurangu: remove extra puts

--- kurangu.rb
+++ kurangu.rb
@@ -2,9 +2,7 @@ require 'rbconfig'
 
 RUBY = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
 
-puts ARGV
 INPUT_FILE = File.expand_path(ARGV[0])
 TRACE_FILE = File.expand_path("trace.rb")
 
-success = system(RUBY, "-r", TRACE_FILE, INPUT_FILE)
-puts "[parent] success: #{success}"
+system(RUBY, "-r", TRACE_FILE, INPUT_FILE)
