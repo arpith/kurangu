f726d047f8e4350a7888285167a6571ed14bd5d2
Arpith Siromoney <arpith@feedreader.co>
2018-02-19 16:36:47 -0500

    trace: remove comment

--- trace.rb
+++ trace.rb
@@ -1,8 +1,8 @@
-trace_return = TracePoint.new(:return) do |t| # event type specification is optional
+trace_return = TracePoint.new(:return) do |t|
   puts "returning #{t.return_value.class} from #{t.defined_class}.#{t.method_id}"
 end
 
-trace_call = TracePoint.new(:call) do |t| # event type specification is optional
+trace_call = TracePoint.new(:call) do |t|
   args = t.binding.eval("local_variables").inject({}) do |vars, name|
     value = t.binding.eval name.to_s
     vars[name] = value.class
