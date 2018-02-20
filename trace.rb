1b171860d4bdd48481866a13551383cca7bb8b46
Arpith Siromoney <arpith@feedreader.co>
2018-02-20 16:25:28 -0500

    trace: generate type annotation

--- trace.rb
+++ trace.rb
@@ -1,5 +1,22 @@
+require 'set'
+
+stack = Hash.new Array.new
+parameter_types = Hash.new Hash.new Set.new
+return_types = Hash.new Array.new
+parameter_list = Hash.new Array.new
+
 trace_return = TracePoint.new(:return) do |t|
-  puts "returning #{t.return_value.class} from #{t.defined_class}.#{t.method_id}"
+  s = "#{t.defined_class}.#{t.method_id}"
+  args = stack[s].pop
+  if args
+    args.each do |arg, type|
+      parameter_types[s][arg].add(type)
+    end
+    return_types[s] << t.return_value.class
+    parameter_list[s] = t.self.method(t.method_id).parameters.map { |a | a[1] }
+    joined_parameters = parameter_list[s].map { |arg| parameter_types[s][arg].to_a.join(" or ") }
+    puts "(#{joined_parameters.join(", ")}) -> #{return_types[s].join(" or ")}"
+  end
 end
 
 trace_call = TracePoint.new(:call) do |t|
@@ -8,7 +25,7 @@ trace_call = TracePoint.new(:call) do |t|
     vars[name] = value.class
     vars
   end
-  puts "calling #{t.defined_class}.#{t.method_id} with #{args}"
+  stack["#{t.defined_class}.#{t.method_id}"] << args
 end
 
 trace_return.enable
