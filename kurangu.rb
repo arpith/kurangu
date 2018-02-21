019ca38c8dd30019fa119b2d0543f4a2855f3373
Arpith Siromoney <arpith@feedreader.co>
2018-02-21 11:34:55 -0500

    kurangu: require input file and run annotations, add new lines for readability

--- kurangu.rb
+++ kurangu.rb
@@ -5,17 +5,19 @@ RUBY = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name
 INPUT_FILE = File.expand_path(ARGV[0])
 TRACE_FILE = File.expand_path("trace.rb")
 
-puts "generating annotations"
+puts "\ngenerating annotations\n"
 system(RUBY, "-r", TRACE_FILE, INPUT_FILE)
 
-puts "the annotations generated are:"
+puts "\nthe annotations generated are:\n"
 File.open("annotations.rb", "r") do |f|
-  f.each_line do |line|
-    puts line
+  f.each_line.with_index do |line, index|
+    if index > 1
+      puts line
+    end
   end
 end
 
 ANNOTATIONS_FILE = File.expand_path("annotations.rb")
 
-puts "running rdl with these annotations"
-system(RUBY, "-r", ANNOTATIONS_FILE, INPUT_FILE)
+puts "\nrunning rdl with these annotations\n"
+system(RUBY, "-r", INPUT_FILE, ANNOTATIONS_FILE)
