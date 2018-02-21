a3e7b3103a2967cd683d792689e70bb78d49f554
Arpith Siromoney <arpith@feedreader.co>
2018-02-21 14:11:11 -0500

    input: add another method to test

--- input.rb
+++ input.rb
@@ -2,8 +2,14 @@ class Test
   def plus(a, b)
     return a + b
   end
+
+  def first(a, b)
+    return a
+  end
 end
 
 t = Test.new
 t.plus(1, 2)
 t.plus('1', '3')
+t.first(1, 3)
+t.first('a', 'b')
