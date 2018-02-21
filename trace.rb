c3cf4920dfd1ff326f0e63e1ecdb1367c696ca75
Arpith Siromoney <arpith@feedreader.co>
2018-02-21 14:12:03 -0500

    trace: write annotations in separate files

--- trace.rb
+++ trace.rb
@@ -1,24 +1,28 @@
 require 'set'
+require 'yaml'
 
 stack = Hash.new Array.new
 parameter_types = Hash.new Hash.new Set.new
 return_types = Hash.new Array.new
 parameter_list = Hash.new Array.new
 signatures = Hash.new
+paths = Set.new
 
-def generate_signature(parameters, parameter_types, return_types)
+def generate_signature(line, parameters, parameter_types, return_types)
   joined_parameters = parameters.map { |arg| parameter_types[arg].to_a.join(" or ") }
-  "(#{joined_parameters.join(", ")}) -> #{return_types.join(" or ")}"
+  "#{line} type '(#{joined_parameters.join(", ")}) -> #{return_types.join(" or ")}'"
 end
 
-def write_annotations(signatures)
-  lines = ["require 'rdl'", "extend RDL::Annotate"]
-  lines.concat signatures.to_a.map{ | name, signature | "#{name}, '#{signature}', typecheck: :now" }
-  IO.write('annotations.rb', lines.join("\n"))
+def write_annotations_paths(paths)
+  IO.write("annotations_paths.txt", paths.to_a.join("\n"))
+end
+
+def write_annotations(path, signatures)
+  IO.write(path, signatures.values.join("\n"))
 end
 
 trace_return = TracePoint.new(:return) do |t|
-  s = "type #{t.defined_class}, :#{t.method_id}"
+  s = "#{t.defined_class}, :#{t.method_id}"
   args = stack[s].pop
   if args
     args.each do |arg, type|
@@ -26,13 +30,16 @@ trace_return = TracePoint.new(:return) do |t|
     end
     return_types[s] << t.return_value.class
     parameter_list[s] = t.self.method(t.method_id).parameters.map { |a | a[1] }
-    signatures[s] = generate_signature(parameter_list[s], parameter_types[s], return_types[s])
-    write_annotations(signatures)
+    line = t.self.method(t.method_id).source_location[1]
+    signatures[s] = generate_signature(line, parameter_list[s], parameter_types[s], return_types[s])
+    path = "#{t.path}.annotations"
+    write_annotations_paths(paths.add(path))
+    write_annotations(path, signatures)
   end
 end
 
 trace_call = TracePoint.new(:call) do |t|
-  s = "type #{t.defined_class}, :#{t.method_id}"
+  s = "#{t.defined_class}, :#{t.method_id}"
   args = t.binding.eval("local_variables").inject({}) do |vars, name|
     value = t.binding.eval name.to_s
     vars[name] = value.class
