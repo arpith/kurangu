91b67d015715620d30e42c4574171e7f8522202f
Arpith Siromoney <arpith@feedreader.co>
2018-02-21 14:12:19 -0500

    kurangu: insert annotations into files :)

--- kurangu.rb
+++ kurangu.rb
@@ -1,4 +1,5 @@
 require 'rbconfig'
+require 'fileutils'
 
 RUBY = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
 
@@ -8,16 +9,53 @@ TRACE_FILE = File.expand_path("trace.rb")
 puts "\ngenerating annotations\n"
 system(RUBY, "-r", TRACE_FILE, INPUT_FILE)
 
+
 puts "\nthe annotations generated are:\n"
-File.open("annotations.rb", "r") do |f|
-  f.each_line.with_index do |line, index|
-    if index > 1
-      puts line
+File.open("annotations_paths.txt", "r") do |f|
+  f.each_line do |annotation_file|
+    puts annotation_file
+    File.open(annotation_file, "r") do |f|
+      f.each_line.with_index do |line, index|
+        if index > 2
+          puts line
+        end
+      end
     end
   end
 end
 
 ANNOTATIONS_FILE = File.expand_path("annotations.rb")
 
-puts "\nrunning rdl with these annotations\n"
-system(RUBY, "-r", INPUT_FILE, ANNOTATIONS_FILE)
+File.open("annotations_paths.txt", "r") do |f|
+  f.each_line do |annotation_path|
+    original_path = annotation_path.chomp('.annotations')
+    annotated_path = "#{original_path}.annotated"
+    puts "\ngenerating annotated file #{annotated_path}\n"
+    lines = ["require 'rdl'\n", "require 'types/core'\n"]
+    annotations = Hash.new
+    File.open(annotation_path, "r") do |f|
+      f.each_line do |line|
+        split = line.split(" ", 2)
+        index = split[0].to_i
+        annotations[index] = "#{split[1]}\n"
+      end
+    end
+    lines << "\n"
+    File.open(original_path, "r") do |f|
+      f.each_line.with_index do |line, index|
+        if annotations.key?(index + 1)
+          lines << "extend RDL::Annotate\n"
+          lines << annotations[index + 1]
+        end
+        lines << line
+      end
+    end
+    IO.write(annotated_path, lines.join())
+    puts "\napplying annotations for #{original_path}\n"
+    FileUtils.mv(annotated_path, original_path)
+  end
+end
+
+
+puts "\nrunning rdl\n"
+system(RUBY, INPUT_FILE)
