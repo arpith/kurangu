dc2f219372c14164e5bf1ae958e80ef4cd237b3a
Arpith Siromoney <arpith@feedreader.co>
2018-02-24 19:13:32 -0500

    trace.rb: only trace files in same directory
    Get input filename from kurangu via stdin
    Only listen to trace if the file is in the same directory as input file
    Also use set to prevent duplicate return types

--- lib/trace.rb
+++ lib/trace.rb
@@ -1,16 +1,17 @@
 require 'set'
-require 'yaml'
+
+INPUT_FILE = $stdin.readline.chomp
 
 stack = Hash.new Array.new
 parameter_types = Hash.new Hash.new Set.new
-return_types = Hash.new Array.new
+return_types = Hash.new Set.new
 parameter_list = Hash.new Array.new
 signatures = Hash.new
 paths = Set.new
 
 def generate_signature(line, parameters, parameter_types, return_types)
   joined_parameters = parameters.map { |arg| parameter_types[arg].to_a.join(" or ") }
-  "#{line} type '(#{joined_parameters.join(", ")}) -> #{return_types.join(" or ")}'"
+  "#{line} type '(#{joined_parameters.join(", ")}) -> #{return_types.to_a.join(" or ")}'"
 end
 
 def write_annotations_paths(dir, paths)
@@ -23,31 +24,35 @@ def write_annotations(path, signatures)
 end
 
 trace_return = TracePoint.new(:return) do |t|
-  s = "#{t.defined_class}, :#{t.method_id}"
-  args = stack[s].pop
-  if args
-    args.each do |arg, type|
-      parameter_types[s][arg].add(type)
+  if File.dirname(t.path) == File.dirname(INPUT_FILE)
+    s = "#{t.defined_class}, :#{t.method_id}"
+    args = stack[s].pop
+    if args
+      args.each do |arg, type|
+        parameter_types[s][arg].add(type)
+      end
+      return_types[s].add(t.return_value.class)
+      parameter_list[s] = t.self.method(t.method_id).parameters.map { |a | a[1] }
+      line = t.self.method(t.method_id).source_location[1]
+      signatures[s] = generate_signature(line, parameter_list[s], parameter_types[s], return_types[s])
+      path = "#{t.path}.annotations"
+      dir = File.dirname(t.path)
+      write_annotations_paths(dir, paths.add(path))
+      write_annotations(path, signatures)
     end
-    return_types[s] << t.return_value.class
-    parameter_list[s] = t.self.method(t.method_id).parameters.map { |a | a[1] }
-    line = t.self.method(t.method_id).source_location[1]
-    signatures[s] = generate_signature(line, parameter_list[s], parameter_types[s], return_types[s])
-    path = "#{t.path}.annotations"
-    dir = File.dirname(t.path)
-    write_annotations_paths(dir, paths.add(path))
-    write_annotations(path, signatures)
   end
 end
 
 trace_call = TracePoint.new(:call) do |t|
-  s = "#{t.defined_class}, :#{t.method_id}"
-  args = t.binding.eval("local_variables").inject({}) do |vars, name|
-    value = t.binding.eval name.to_s
-    vars[name] = value.class
-    vars
+  if File.dirname(t.path) == File.dirname(INPUT_FILE)
+    s = "#{t.defined_class}, :#{t.method_id}"
+    args = t.binding.eval("local_variables").inject({}) do |vars, name|
+      value = t.binding.eval name.to_s
+      vars[name] = value.class
+      vars
+    end
+    stack[s] << args
   end
-  stack[s] << args
 end
 
 trace_return.enable
